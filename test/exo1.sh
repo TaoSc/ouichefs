26205e47024464238625e30690e05728a1a47103
Tao Schreiner <taoschreiner@outlook.com>
2022-05-26 19:35:30 +0200

    adds test file for step1 and mkfs change for step3

---
+++ test/exo1.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+insmod /share/ouichefs.ko
+mkdir /wish
+mount -t ouichefs /dev/sdc /wish
+cd /wish
+
+echo "ver1" > test_file1
+cat test_file1
+echo "ver1" > test_file2
+cat test_file2
+echo "ver2" > test_file1
+echo "ver3" > test_file1
+
+cd /
+umount ouichefs
+
+mount -t ouichefs /dev/sdc /wish
+cd /wish
+
+cat test_file1
+cat test_file2
+echo "ver2" > test_file2
+cat test_file2
+
+cd /
+umount ouichefs
+
+echo
+echo "Doit afficher, dans l'ordre :"
+echo ver1
+echo ver1
+echo ver3
+echo ver1
+echo ver2
