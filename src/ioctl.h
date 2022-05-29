6e06664ccc0a2d74d7bca52ab804530bbcd6993f
BestJediInTheGalaxy <andrey.prostakov.1999@gmail.com>
2022-05-29 16:33:42 +0200

    Formatting

--- src/ioctl.h
+++ src/ioctl.h
@@ -3,10 +3,10 @@
 //À tester
 
 #define IOCTL_NAME  "ouichefs_ioctl"
-#define CHANGE_VER  _IOWR('0',1,int32_t) 
-#define NEW_LATEST _IOWR('1',2,int32_t) 
+#define CHANGE_VER  _IOWR('0', 1, int32_t) 
+#define NEW_LATEST _IOWR('1', 2, int32_t) 
 
 struct ioctl_request{
-    int ino;
-    int nb_version;
+        int ino;
+        int nb_version;
 };
