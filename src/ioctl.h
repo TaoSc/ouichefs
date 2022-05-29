048e91e2aabde2285ecf7f9f170a136d6eed37df
Tao Schreiner <taoschreiner@outlook.com>
2022-05-29 20:57:38 +0200

    clean up ioctl

--- src/ioctl.h
+++ src/ioctl.h
@@ -1,10 +1,8 @@
-// #include <asm-generic/ioctl.h>
-// Il faut rajouter un header pour les macros mais je sais pas si c'est le bon,
-//À tester
+#include <asm-generic/ioctl.h>
 
 #define IOCTL_NAME  "ouichefs_ioctl"
 #define CHANGE_VER  _IOWR('0', 1, int32_t) 
-#define NEW_LATEST _IOWR('1', 2, int32_t) 
+#define NEW_LATEST  _IOWR('1', 2, int32_t) 
 
 struct ioctl_request {
 	int ino;
